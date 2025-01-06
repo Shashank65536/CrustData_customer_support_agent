e5279bf723b94e600e45ed679a1ba67e3d7fe069
unknown <shashankbidwai6@gmail.com>
2025-01-06 00:12:42 -0500

    created chat broker and controller

---
+++ backend/src/main/java/com/chatapp/chatwebsocket/controller/ChatController.java
@@ -0,0 +1,22 @@
+package com.chatapp.chatwebsocket.controller;
+
+import com.chatapp.chatwebsocket.beans.ChatMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
+    @MessageMapping("/chat/guest")
+    @SendTo("/topic/chat")
+    public ChatMessage handleChatMessage(@DestinationVariable String chatId, ChatMessage message) {
+
+        System.out.print(message);
+        return  new ChatMessage(message.getMessage());
+    }
+}
